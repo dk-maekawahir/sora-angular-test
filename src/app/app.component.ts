861eebd889ac8579b1ea4c49450301300d50aba8
MAEKAWA Hiroshi <hiroshi1.maekawa@daikin.co.jp>
2021-09-24 09:26:03 +0900

    ➕  add sora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ConnectionOptions, ConnectionPublisher, default as Sora } from 'sora-js-sdk';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const sora = Sora.connection("ws://192.0.2.100:5000/signaling", true);
+  }
   title = 'sora-ng-problem';
 }
